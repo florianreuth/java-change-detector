ed14596652b2c2844edbe74f5aef5ffa7315df47
Florian Reuth <git@florianreuth.de>
2026-08-02 12:58:10 +0200

    Update copyright header
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- src/main/java/de/florianreuth/jcd/Monitor.java
+++ src/main/java/de/florianreuth/jcd/Monitor.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of java-change-detector - https://github.com/florianreuth/java-change-detector
- * Copyright (C) 2026 Florian Reuth <git@florianreuth.de> and contributors
+ * Copyright (C) 2026 Florian Reuth <git@florianreuth.de>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
